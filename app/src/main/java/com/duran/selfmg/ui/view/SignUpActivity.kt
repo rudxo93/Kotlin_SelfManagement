15574595d02c94cbbaceba429dffde4fb83e40d7
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-08-14 17:58:43 +0900

    회원가입 -> 비밀번호, 비밀번호 확인 구현 -> 비밀번호 정규식에 따른 비밀번호 / 비밀번호 입력 시 조건에 따른 멘트 / 비밀번호 입력 후 비밀번호와 일치한지 조건에 따른 멘트

--- app/src/main/java/com/duran/selfmg/ui/view/SignUpActivity.kt
+++ app/src/main/java/com/duran/selfmg/ui/view/SignUpActivity.kt
@@ -32,11 +32,14 @@ class SignUpActivity : AppCompatActivity() {
         initToolBarSetting()
         // 이메일 입력
         initEmail()
-        // 이메일 입력 시 엔터키 클릭시 패드 숨기기
-        setListenerToEditText()
+        // 비밀번호 입력
+        initPassword()
+        // 비밀번호 입력 확인
+        initPasswordCheck()
 
     }
 
+    // =======================================이메일=======================================
     /* 이메일 입력 */
     private fun initEmail() {
         binding.edUsedEmail.addTextChangedListener(object: TextWatcher {
@@ -52,14 +55,17 @@ class SignUpActivity : AppCompatActivity() {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 // 우선 이메일 입력이 비었는지 구문
                 if(email.isEmpty()){
+                    // 이메일 입력이 비었다면 -> 이메일 입력해주세요 문구
                     binding.liEmailCheckAlarm.visibility = View.VISIBLE
                     binding.ivEmailAlarm.setImageResource(R.drawable.ic_cancel)
                     binding.tvEmailAlarm.text = "이메일을 입력해주세요"
                 } else if(pattern.matcher(email).matches()) {
+                    // 이메일 패턴을 가져와서 패턴이 맞다면
                     binding.liEmailCheckAlarm.visibility = View.VISIBLE
                     binding.ivEmailAlarm.setImageResource(R.drawable.ic_check_circle)
                     binding.tvEmailAlarm.text = "성공"
                 } else {
+                    // 이메일 입력이 되어있고 패턴이 틀리다면
                     binding.liEmailCheckAlarm.visibility = View.VISIBLE
                     binding.ivEmailAlarm.setImageResource(R.drawable.ic_cancel)
                     binding.tvEmailAlarm.text = "이메일 형식이 맞지 않습니다."
@@ -72,11 +78,12 @@ class SignUpActivity : AppCompatActivity() {
             }
 
         })
+        setListenerToEmailEditText()
 
     }
 
     /* 이메일 엔터키 클릭 시 키패드 내리기 */
-    private fun setListenerToEditText() {
+    private fun setListenerToEmailEditText() {
         binding.edUsedEmail.setOnKeyListener { _, keyCode, event ->
             // 엔터키 액션
             if(event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
@@ -89,7 +96,125 @@ class SignUpActivity : AppCompatActivity() {
         }
 
     }
-    /* 이메일 엔터키 클릭 시 키패드 내리기 */
+    // =======================================이메일=======================================
+
+    // =======================================비밀번호=======================================
+    /* 비밀번호 입력 */
+    private fun initPassword() {
+        binding.edCreatePassword.addTextChangedListener(object: TextWatcher {
+            // 비밀번호 정규식
+            val pwPattern = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&.])[A-Za-z[0-9]$@$!%*#?&.]{8,20}$" // 영문, 숫자, 특수문자 8 ~ 20자 패턴
+            val pattern = Pattern.compile(pwPattern)
+            val pw = binding.edCreatePassword.text
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(pw.isEmpty()){
+                    // 비밀번호 입력이 비어있다면 -> 정규식대로 입력하라고 메세지
+                    binding.liPw.visibility = View.VISIBLE
+                    binding.ivPwAlarm.setImageResource(R.drawable.ic_cancel)
+                    binding.tvPwAlarm.text = "비밀번호를 입력해주세요"
+                } else if(pattern.matcher(pw).matches()) {
+                    // 비밀번호 정규식 패턴이 맞다면
+                    binding.liPw.visibility = View.VISIBLE
+                    binding.ivPwAlarm.setImageResource(R.drawable.ic_check_circle)
+                    binding.tvPwAlarm.text = "사용 가능한 비밀번호입니다."
+                } else {
+                    // 비밀번호 입력이 되어있고 정규식 패턴이 틀리다면
+                    binding.liPw.visibility = View.VISIBLE
+                    binding.ivPwAlarm.setImageResource(R.drawable.ic_cancel)
+                    binding.tvPwAlarm.text = "비밀번호는 영문, 숫자, 특수문자 포함 8~20자입니다."
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+        setListenerToPwEditText()
+    }
+
+    /* 비밀번호 입력 엔터키 클릭 시 키패드 내리기 */
+    private fun setListenerToPwEditText() {
+        binding.edCreatePassword.setOnKeyListener { v, keyCode, event ->
+            // 엔터키 액션
+            if(event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                // 키패드 내리기
+                val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(binding.edCreatePassword.windowToken, 0)
+                true
+            }
+            false
+        }
+    }
+    // =======================================비밀번호=======================================
+
+    // =======================================비밀번호 확인=======================================
+    private fun initPasswordCheck() {
+        //
+        binding.edCreatePasswordCheck.addTextChangedListener(object: TextWatcher {
+            // 비밀번호 정규식
+            val pwPattern = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&.])[A-Za-z[0-9]$@$!%*#?&.]{8,20}$" // 영문, 숫자, 특수문자 8 ~ 20자 패턴
+            val pattern = Pattern.compile(pwPattern)
+            val pwCheck = binding.edCreatePasswordCheck.text
+            val pw = binding.edCreatePassword.text
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(pwCheck.isEmpty()){
+                    // 비밀번호 확인이 비어있다면 -> 정규식대로 입력하라고 메세지
+                    binding.liPwCheck.visibility = View.VISIBLE
+                    binding.ivPwCheckAlarm.setImageResource(R.drawable.ic_cancel)
+                    binding.tvPwCheckAlarm.text = "비밀번호를 입력해주세요"
+                } else if(pattern.matcher(pwCheck).matches()) {
+                    // 비밀번호 정규식 패턴이 맞다면 -> 비밀번호가 일치하는지?
+                    if(pw.toString().equals(pwCheck.toString())) {
+                        binding.liPwCheck.visibility = View.VISIBLE
+                        binding.ivPwCheckAlarm.setImageResource(R.drawable.ic_check_circle)
+                        binding.tvPwCheckAlarm.text = "비밀번호가 일치합니다."
+                    } else { // 위와 비밀번호가 일치하지 않다면?
+                        binding.liPwCheck.visibility = View.VISIBLE
+                        binding.ivPwCheckAlarm.setImageResource(R.drawable.ic_cancel)
+                        binding.tvPwCheckAlarm.text = "비밀번호가 일치하지 않습니다."
+                    }
+                } else {
+                    // 비밀번호 입력이 되어있고 정규식 패턴이 틀리다면
+                    binding.liPwCheck.visibility = View.VISIBLE
+                    binding.ivPwCheckAlarm.setImageResource(R.drawable.ic_cancel)
+                    binding.tvPwCheckAlarm.text = "비밀번호는 영문, 숫자, 특수문자 포함 8~20자입니다."
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+
+        setListenerToPwCheckEditText()
+    }
+
+    /* 비밀번호 확인 엔터키 클릭 시 키패드 내리기 */
+    private fun setListenerToPwCheckEditText() {
+        binding.edCreatePasswordCheck.setOnKeyListener { v, keyCode, event ->
+            // 엔터키 액션
+            if(event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                // 키패드 내리기
+                val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(binding.edCreatePasswordCheck.windowToken, 0)
+                true
+            }
+            false
+        }
+    }
+    // =======================================비밀번호 확인=======================================
 
     /* 회원가입 성공 시 Login 페이지로 이동 */
     private fun moveLogin() {
@@ -98,6 +223,7 @@ class SignUpActivity : AppCompatActivity() {
     }
     /* 회원가입 성공 시 Login 페이지로 이동 */
 
+    // =======================================회원가입 툴바=======================================
     /* toolbar */
     private fun initToolBarSetting() {
         val toolbar = binding.toolbar
@@ -118,5 +244,5 @@ class SignUpActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
-    /* toolbar */
+    // =======================================회원가입 툴바=======================================
 }
