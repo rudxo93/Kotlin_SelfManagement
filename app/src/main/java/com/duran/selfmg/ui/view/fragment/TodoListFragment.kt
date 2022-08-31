06ca00db686c9e246a9ad482f23db0d5f30170c1
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-08-31 12:25:18 +0900

    PlanFragment -> TodoList 프래그먼트에서 add버튼 클릭시 Todo 추가할 수 있는 레이아웃 구조 변경(linearLayout -> dialog)

--- app/src/main/java/com/duran/selfmg/ui/view/fragment/TodoListFragment.kt
+++ app/src/main/java/com/duran/selfmg/ui/view/fragment/TodoListFragment.kt
@@ -1,36 +1,26 @@
 package com.duran.selfmg.ui.view.fragment
 
-import android.content.Context
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
-import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.duran.selfmg.R
-import com.duran.selfmg.data.model.TodoListEntity
 import com.duran.selfmg.databinding.FragmentTodoListBinding
-import com.duran.selfmg.ui.adapter.TodoListAdapter
-import com.duran.selfmg.ui.viewmodel.TodoListVIewModel
-import java.text.SimpleDateFormat
+import com.duran.selfmg.ui.view.activity.LoginActivity
 
 class TodoListFragment : Fragment() {
 
     private lateinit var binding: FragmentTodoListBinding
 
     private val btnAddTodoList by lazy { binding.btnAddTravelPlan }
-    private val addTodoListLayout by lazy { binding.linearAddTodoListView }
-    private val btnTodoListCancel by lazy { binding.btnTodoListCancel }
-    private val btnAddTodo by lazy { binding.btnTodoListAdd }
-    private val todoContent by lazy { binding.edAddTodoListContent }
 
-    private lateinit var todoViewModel: TodoListVIewModel
-    lateinit var todoListAdapter: TodoListAdapter
+    /*private lateinit var todoViewModel: TodoListVIewModel
+    lateinit var todoListAdapter: TodoListAdapter*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,59 +38,28 @@ class TodoListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        todoViewModel = ViewModelProvider(this)[TodoListVIewModel::class.java]
+        /*todoViewModel = ViewModelProvider(this)[TodoListVIewModel::class.java]*/
 
         /*todoViewModel.getAllTodoList()*/
 
         initBtnAddTodoList() // 할일 추가하기 버튼
-        initBtnAddTodoListCancel() // 할일 추가하기 -> 취소하기 버튼
-        initBtnAddTodoInsert() // 할 일 추가하기에서 추가하기 버튼 클릭 -> DB에 저장하기
-        initRecyclerViewSetting()
+        /*initRecyclerViewSetting()*/
     }
 
     // ======================================= 할일 추가하기 버튼 클릭 =======================================
     private fun initBtnAddTodoList() {
-        btnAddTodoList.setOnClickListener {
-            btnAddTodoList.isVisible = false
-            addTodoListLayout.isVisible = true
-        }
-    }
+        btnAddTodoList.setOnClickListener{
+            val addTodoDialogView = LayoutInflater.from(context).inflate(R.layout.dialog_add_todo, null)
+            val addTodoBuilder = AlertDialog.Builder(context)
+                .setView(addTodoDialogView)
+                .setTitle("할 일 추가하기")
 
-    // ======================================= 할일 추가하기에서 취소하기 버튼 클릭 =======================================
-    private fun initBtnAddTodoListCancel() {
-        btnTodoListCancel.setOnClickListener {
-            btnAddTodoList.isVisible = true
-            addTodoListLayout.isVisible = false
-        }
-    }
-
-    // ======================================= 할일 추가하기 버튼 클릭 -> DB에 저장하기 =======================================
-    private fun initBtnAddTodoInsert() {
-        btnAddTodo.setOnClickListener {
-            val content = todoContent.text.toString()
-            val currentDate =
-                SimpleDateFormat("yyyy-MM-dd HH:mm").format(System.currentTimeMillis())
-
-            if (todoContent.text.isNotEmpty()) {
-                todoViewModel.todoInsert(TodoListEntity(content, currentDate, false))
-                Toast.makeText(context, "할 일이 추가되었습니다.", Toast.LENGTH_SHORT).show()
-                todoContent.text.clear()
-                // 키패드 내리기
-                val imm =
-                    context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.hideSoftInputFromWindow(view?.windowToken, 0)
-                addTodoListLayout.isVisible = false // 할일 추가하기 레이아웃 숨기기
-                btnAddTodoList.isVisible = true // add버튼 보이게 하기
-            } else {
-                Toast.makeText(context, "할 일을 작성 후 추가해주세요.", Toast.LENGTH_SHORT).show()
-            }
-
-
+            addTodoBuilder.show()
         }
     }
 
     // ======================================= Todo리사이클러뷰 =======================================
-    private fun initRecyclerViewSetting() {
+ /*   private fun initRecyclerViewSetting() {
         todoViewModel.todoList.observe(viewLifecycleOwner) {
             todoListAdapter.update(it)
         }
@@ -108,6 +67,25 @@ class TodoListFragment : Fragment() {
         todoListAdapter = context?.let { TodoListAdapter(it) }!!
         binding.rvTodolist.layoutManager = LinearLayoutManager(context)
         binding.rvTodolist.adapter = todoListAdapter
-    }
+
+        todoListAdapter.setItemClickListener(object : TodoListAdapter.ItemClickListener {
+            override fun onClick(view: View, position: Int, itemId: Long) {
+                Toast.makeText(context, "$itemId", Toast.LENGTH_SHORT).show()
+                Log.e("tag", "$itemId")
+                val todo = todoViewModel.getTodo(itemId)
+                Log.e("tag", "${todo.isActive}")
+            }
+
+        })
+
+        todoListAdapter.setItemCheckBoxClickListener(object : TodoListAdapter.ItemCheckBoxClickListener {
+            override fun onClick(view: View, position: Int, itemId: Long) {
+                CoroutineScope(Dispatchers.IO).launch {
+                    val todo = todoViewModel.getTodo(itemId)
+                }
+            }
+        })
+    }*/
+
 
 }
