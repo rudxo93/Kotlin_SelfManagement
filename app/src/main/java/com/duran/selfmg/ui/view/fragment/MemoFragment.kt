e54919362cf2548945aefb262ebfc81571294a85
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-09-06 16:38:36 +0900

    MemoFragment -> Memo프래그먼트에서의 레이아웃 변경과 메모 추가하기 레이아웃 재구성(coordinatorlayout 사용하기)

--- app/src/main/java/com/duran/selfmg/ui/view/fragment/MemoFragment.kt
+++ app/src/main/java/com/duran/selfmg/ui/view/fragment/MemoFragment.kt
@@ -9,12 +9,12 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.duran.selfmg.R
 import com.duran.selfmg.databinding.FragmentMemoBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MemoFragment : Fragment() {
 
     private lateinit var binding : FragmentMemoBinding
 
-    private val btnAddMemo by lazy { binding.btnAddMemoList }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,13 +32,46 @@ class MemoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initBtnAddMemo()
+        initBottomBehaviorEvent()
 
     }
 
-    // ======================================= Add 버튼 클릭 =======================================
-    private fun initBtnAddMemo() {
+    // ======================================= Bottom Sheet Behavior =======================================
+    private fun initBottomBehaviorEvent() {
+        val bottomBehavior = BottomSheetBehavior.from(binding.memoBottomSheet.root)
 
+        bottomBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+
+        bottomBehavior.apply {
+            addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    /*when(newState) {
+                        BottomSheetBehavior.STATE_COLLAPSED -> {} // 접힘
+                        BottomSheetBehavior.STATE_EXPANDED -> {} // 펼쳐짐
+                        BottomSheetBehavior.STATE_HIDDEN -> {}    //숨겨짐
+                        BottomSheetBehavior.STATE_HALF_EXPANDED -> {} //절반 펼쳐짐
+                        BottomSheetBehavior.STATE_DRAGGING -> {}  //드래그하는 중
+                        BottomSheetBehavior.STATE_SETTLING -> {}  //(움직이다가) 안정화되는 중
+                    }*/
+                }
+
+                override fun onSlide(bottomSheet: View, slideOffset: Float) { // 화살표 전환
+                    //슬라이드 될때 offset / hide -1.0 ~ collapsed 0.0 ~ expended 1.0
+                    binding.memoBottomSheet.apply {
+                        tvBottomWriteMemoArrow.rotation = slideOffset * -180F
+                    }
+                }
+            })
+        }
+
+        binding.memoBottomSheet.btnBottomWriteMemo.setOnClickListener {
+            bottomBehavior.state = if (bottomBehavior.state == BottomSheetBehavior.STATE_COLLAPSED) { // bottomBehavior상태가 접힌 상태라면
+                BottomSheetBehavior.STATE_EXPANDED // 펼쳐준다.
+            } else
+                BottomSheetBehavior.STATE_COLLAPSED // 아니라면 -> 펼쳐져있다면 접어준다.
+        }
     }
+
+
 
 }
