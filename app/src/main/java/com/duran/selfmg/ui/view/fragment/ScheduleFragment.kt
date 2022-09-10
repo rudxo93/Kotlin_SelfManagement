8bb031d8dc36c15448dcd2f19b21d0391901d077
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-09-10 21:20:14 +0900

    ScheduleFragment -> Schedule 레이아웃 간단하게 구상해보기...(시간표처럼 만들고 싶은데 어떻게 해야할지 고민해볼것...)

--- app/src/main/java/com/duran/selfmg/ui/view/fragment/ScheduleFragment.kt
+++ app/src/main/java/com/duran/selfmg/ui/view/fragment/ScheduleFragment.kt
@@ -1,27 +1,47 @@
 package com.duran.selfmg.ui.view.fragment
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.duran.selfmg.R
+import com.duran.selfmg.databinding.FragmentScheduleBinding
+import com.duran.selfmg.ui.view.activity.LoginActivity
 
 class ScheduleFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    lateinit var binding: FragmentScheduleBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_schedule, container, false)
+
+        initToolBarSetting()
+
+        return binding.root
     }
+
+
+    // ======================================= 일정 시간표 툴바 =======================================
+    private fun initToolBarSetting() {
+        val toolbar = binding.toolbarSchedule
+        toolbar.inflateMenu(R.menu.item_toolbar_schedule)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.item_toolbar_schedule, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
 
 }
