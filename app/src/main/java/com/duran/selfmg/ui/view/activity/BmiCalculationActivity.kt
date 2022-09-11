85aac91f37ba47ae6116fbe5143fa9fb7ec8c415
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-09-11 18:23:41 +0900

    HealthFragment -> BMI수치에 대한 정보 레이아웃 / 측정하기 클릭 시 신장과 체중에 입력한 값 확인하기 / 초기화 클릭 시 입력창 clear

--- app/src/main/java/com/duran/selfmg/ui/view/activity/BmiCalculationActivity.kt
+++ app/src/main/java/com/duran/selfmg/ui/view/activity/BmiCalculationActivity.kt
@@ -3,34 +3,50 @@ package com.duran.selfmg.ui.view.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import com.duran.selfmg.R
 import com.duran.selfmg.databinding.ActivityBmiCalculationBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class BmiCalculationActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityBmiCalculationBinding
 
-    private val btnBmiInfo by lazy { binding.btnBmiInfo }
-    private val textViewBmiInfo by lazy { binding.tvBmiInfoText }
+    private val btnBmiInfo by lazy { binding.btnBmiInfo } // bmi에 대해서 정보보기
+    private val textViewBmiInfo by lazy { binding.tvBmiInfoText } // bmi 정보 textView
+    private val inputHeightText by lazy { binding.edBmiInputHeight.text } // 신장 입력
+    private val inputWeightText by lazy { binding.edBmiInputWeight.text } // 체중 입력
+    private val btnStartResult by lazy { binding.btnStartBmiResult } // 측정하기 버튼
+    private val btnBmiClear by lazy { binding.btnStartBmiClear } // Bmi 초기화 버튼
 
     private var isInfoOpen = false // Bmi 정보버튼은 닫혀있다.
+
+    private lateinit var auth: FirebaseAuth // 계정인증
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_bmi_calculation)
 
+        auth = FirebaseAuth.getInstance()
+
         initToolBarSetting()
         initBtnBmiInfoClick()
+        initClickBtnBmiResult()
+        initClickBtnClear()
+
+        /*var authUser = auth.currentUser?.email // 현재 접속중인 유저 이메일 정보 나중에 사용*/
 
     }
 
-    // ======================================= 일정 시간표 툴바 =======================================
+    // ======================================= MBI 계산하기 툴바 =======================================
     private fun initToolBarSetting() {
         val toolbar = binding.toolbarBmiCalculation
 
@@ -65,5 +81,28 @@ class BmiCalculationActivity : AppCompatActivity() {
             textViewBmiInfo.visibility = View.VISIBLE
         }
         isInfoOpen = !isInfoOpen
+    }
+
+    // ======================================= BMI 측정하기 버튼 클릭 =======================================
+    private fun initClickBtnBmiResult() {
+        btnStartResult.setOnClickListener {
+            if(inputHeightText.isEmpty()) {
+                Toast.makeText(this, "신장 입력창이 비었습니다.", Toast.LENGTH_SHORT).show()
+            } else if (inputWeightText.isEmpty()) {
+                Toast.makeText(this, "체중 입력창이 비었습니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.e("tag", inputHeightText.toString())
+                Log.e("tag", inputWeightText.toString())
+            }
+        }
+    }
+
+    // ======================================= 초기화 버튼 클릭 =======================================
+    private fun initClickBtnClear() {
+        btnBmiClear.setOnClickListener {
+            inputHeightText.clear()
+            inputWeightText.clear()
+            Toast.makeText(this, "신장과 체중입력을 초기화했습니다.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
