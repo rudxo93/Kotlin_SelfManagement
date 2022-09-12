cd1b000f500d3f7ad62db7c0d705a6446c158da8
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-09-12 16:06:12 +0900

    HealthFragment -> 측정하기 클릭 시 BMI수치와 BMI수치에 따른 판정값 보여주기 / 수치와 판정값에 따른 글 색상변경 / 측정하기와 초기화 시 수치와 판정값 보여주는 layout Visible 처리 / 입력이 끝났다면 키보드 내려주기

--- app/src/main/java/com/duran/selfmg/ui/view/activity/BmiCalculationActivity.kt
+++ app/src/main/java/com/duran/selfmg/ui/view/activity/BmiCalculationActivity.kt
@@ -1,10 +1,13 @@
 package com.duran.selfmg.ui.view.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.duran.selfmg.R
 import com.duran.selfmg.databinding.ActivityBmiCalculationBinding
@@ -24,6 +27,9 @@ class BmiCalculationActivity : AppCompatActivity() {
     private val inputWeightText by lazy { binding.edBmiInputWeight.text } // 체중 입력
     private val btnStartResult by lazy { binding.btnStartBmiResult } // 측정하기 버튼
     private val btnBmiClear by lazy { binding.btnStartBmiClear } // Bmi 초기화 버튼
+    private val resultLinearLayout by lazy { binding.linearBmiResult } // bmi 측정하기 클릭시 나오는 레이아웃
+    private val resultBmiNum by lazy { binding.tvBmiResultNumber } // 측정된 bmi 지수
+    private val resultBmiRange by lazy { binding.tvBmiResultRange } // 측정된 bmi 지수 판정
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -69,7 +75,7 @@ class BmiCalculationActivity : AppCompatActivity() {
 
     // ======================================= BMI 설명보기 버튼을 토글버튼으로 설명 숨김과 보여주기 =======================================
     private fun toggleButton() {
-        if(isInfoOpen) {
+        if (isInfoOpen) {
             textViewBmiInfo.visibility = View.GONE
         } else {
             textViewBmiInfo.visibility = View.VISIBLE
@@ -99,6 +105,7 @@ class BmiCalculationActivity : AppCompatActivity() {
         btnBmiClear.setOnClickListener {
             inputHeightText.clear()
             inputWeightText.clear()
+            resultLinearLayout.visibility = View.GONE
             Toast.makeText(this, "신장과 체중입력을 초기화했습니다.", Toast.LENGTH_SHORT).show()
         }
     }
@@ -109,16 +116,35 @@ class BmiCalculationActivity : AppCompatActivity() {
         val weight = inputWeightText.toString().toDouble()
         val bmi = bmiCalculation(height, weight)// 계산된 BMI지수
         if (bmi < 18.5) {
-            Toast.makeText(this, "저체중입니다.$bmi", Toast.LENGTH_SHORT).show()
+            resultBmiNum.text = bmi.toString()
+            resultBmiNum.setTextColor(Color.parseColor("#2D81FF"))
+            resultBmiRange.text = "저체중"
+            resultBmiRange.setTextColor(Color.parseColor("#2D81FF"))
         } else if (bmi >= 18.5 && bmi < 23) {
-            Toast.makeText(this, "정상입니다.$bmi", Toast.LENGTH_SHORT).show()
+            resultBmiNum.text = bmi.toString()
+            resultBmiNum.setTextColor(Color.parseColor("#2D81FF"))
+            resultBmiRange.text = "정상"
+            resultBmiRange.setTextColor(Color.parseColor("#2D81FF"))
         } else if (bmi >= 23 && bmi < 25) {
-            Toast.makeText(this, "과체중입니다.$bmi", Toast.LENGTH_SHORT).show()
+            resultBmiNum.text = bmi.toString()
+            resultBmiNum.setTextColor(Color.parseColor("#FF000000"))
+            resultBmiRange.text = "과체중"
+            resultBmiRange.setTextColor(Color.parseColor("#FF000000"))
         } else if (bmi >= 25 && bmi < 30) {
-            Toast.makeText(this, "경도비만입니다.$bmi", Toast.LENGTH_SHORT).show()
+            resultBmiNum.text = bmi.toString()
+            resultBmiNum.setTextColor(Color.parseColor("#FF000000"))
+            resultBmiRange.text = "경도비만"
+            resultBmiRange.setTextColor(Color.parseColor("#FF000000"))
         } else {
-            Toast.makeText(this, "중등도비만입니다.$bmi", Toast.LENGTH_SHORT).show()
+            resultBmiNum.text = bmi.toString()
+            resultBmiNum.setTextColor(Color.parseColor("#8A3131"))
+            resultBmiRange.text = "중등도비만"
+            resultBmiRange.setTextColor(Color.parseColor("#8A3131"))
         }
+
+        resultLinearLayout.visibility = View.VISIBLE
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
 
     }
 
