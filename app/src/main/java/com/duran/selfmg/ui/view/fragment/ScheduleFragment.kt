9e5b9f91bde841bbc3e843dbc7f140be43bdedbd
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-09-19 15:17:09 +0900

    ScheduleFragment -> MaterialCalendarView에서 토요일은 파란색 / 일요일은 빨간색 / 당일은 초록색으로 두껍게 색 바꿔주기

--- app/src/main/java/com/duran/selfmg/ui/view/fragment/ScheduleFragment.kt
+++ app/src/main/java/com/duran/selfmg/ui/view/fragment/ScheduleFragment.kt
@@ -1,17 +1,22 @@
 package com.duran.selfmg.ui.view.fragment
 
-import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
-import android.view.*
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import android.text.style.ForegroundColorSpan
+import android.text.style.RelativeSizeSpan
+import android.text.style.StyleSpan
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.duran.selfmg.R
 import com.duran.selfmg.databinding.FragmentScheduleBinding
-import com.duran.selfmg.ui.view.activity.AddScheduleActivity
-import com.duran.selfmg.ui.view.activity.LoginActivity
 import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.prolificinteractive.materialcalendarview.CalendarMode
+import com.prolificinteractive.materialcalendarview.DayViewDecorator
+import com.prolificinteractive.materialcalendarview.DayViewFacade
 import java.util.*
 
 class ScheduleFragment : Fragment() {
@@ -43,6 +48,68 @@ class ScheduleFragment : Fragment() {
             .setMaximumDate(CalendarDay.from(2030, 11, 31))
             .setCalendarDisplayMode(CalendarMode.MONTHS)
             .commit()
+
+        calendar.addDecorators(SaturdayDecorator(),SundayDecorator(), OneDayDecorator())
     }
 
+}
+
+// ======================================= CalendarView의 토요일 날짜 색 =======================================
+class SaturdayDecorator : DayViewDecorator {
+
+    private companion object {
+        val calendar: Calendar = Calendar.getInstance()
+    }
+
+    override fun shouldDecorate(day: CalendarDay?): Boolean {
+        day?.copyTo(calendar)
+        val weekDay = calendar.get(Calendar.DAY_OF_WEEK)
+        return weekDay == Calendar.SATURDAY
+    }
+
+    override fun decorate(view: DayViewFacade?) {
+        view!!.addSpan(ForegroundColorSpan(Color.BLUE))
+    }
+}
+
+// ======================================= CalendarView의 일요일 날짜 색 =======================================
+class SundayDecorator : DayViewDecorator {
+
+    private companion object {
+        val calendar: Calendar = Calendar.getInstance()
+    }
+
+    override fun shouldDecorate(day: CalendarDay?): Boolean {
+        day?.copyTo(calendar)
+        val weekDay = calendar.get(Calendar.DAY_OF_WEEK)
+        return weekDay == Calendar.SUNDAY
+    }
+
+    override fun decorate(view: DayViewFacade?) {
+        view!!.addSpan(ForegroundColorSpan(Color.RED))
+    }
+
+}
+
+// ======================================= CalendarView의 당일 날짜 색 =======================================
+class OneDayDecorator : DayViewDecorator {
+    private var date: CalendarDay
+
+    init {
+        date = CalendarDay.today()
+    }
+
+    override fun shouldDecorate(day: CalendarDay?): Boolean {
+        return day?.equals(date)!!
+    }
+
+    override fun decorate(view: DayViewFacade?) {
+        view?.addSpan(StyleSpan(Typeface.BOLD))
+        view?.addSpan(RelativeSizeSpan(1.4f))
+        view?.addSpan(ForegroundColorSpan(Color.GREEN))
+    }
+
+    fun setDate(date: Date) {
+        this.date = CalendarDay.from(date)
+    }
 }
